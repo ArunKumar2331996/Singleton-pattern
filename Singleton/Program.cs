b37ee30f569ed01a55b44c5c9e4a3d692164d609
admin <admin@DESKTOP-O74C053>
2022-07-25 17:11:39 +0530

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    // This sealed class will prevent inheritance.
+    sealed class Program
+    {
+        //  This private prooperty helps us to create only one object during run time.
+        private static object Instance = null;
+        private Program()
+        {
+            // This private constructor will not allow us to create new object.
+        }
+
+        static Program()
+        {
+            // This static constructor helps us to create new object only one time
+            // when application starts running.
+
+            if (Instance == null) 
+                Instance = new Program();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }       
+    }
+}
